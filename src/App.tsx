3ce76ebd88d28d7803c24410b6de6f0bf7fba078
VikremBhagi <vikrembhagi@gmail.com>
2021-04-02 16:39:53 -0500

    Added navlink

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,51 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  NavLink,
+  useLocation
 } from "react-router-dom";
 
 function App() {
-  const [masterIdeaList, setMasterIdeaList] = useState([]);
-  const [isBusy, setBusy] = useState(true);
-  const sampleIdeaData = SampleIdea()
 
-
+   //Styles 
   const appHeader = {
     display: "flex",
-    fontSize: "48px",
     height:"10vh",
     justifyContent: "center",
+    flexDirection:"column" as "column",
     margin: "48px"
   };
+
+  const appTitle = {
+    fontSize: "48px"
+  }
+
+  const navItemContainer = {
+    padding:16,
+  }
+
+  const navItem = {
+    color:"white"
+  }
+
+  const navItemSelected = {
+    color:"yellow",
+    textDecoration:"underline"
+  }
+
+  const navHeader = {
+    display:"flex",
+    flexDirection:"row" as "row",
+    width:"100%",
+    justifyContent: "center",
+    fontSize:20
+  }
 
   const appContainer = {
     display:"flex"
   }
 
+ //Firebase functions 
   if (!Firebase.apps.length) {
     Firebase.initializeApp(config)
 }
@@ -45,7 +69,7 @@ function App() {
         if (snapshot && snapshot.exists()) {
           setMasterIdeaList(snapshot.val())
           console.log(masterIdeaList)
-          setBusy(false)
+          setLoadingIdeas(false)
         }})
   }
 
@@ -55,6 +79,7 @@ function App() {
       .set(sampleIdeaData)
   };
 
+
   function addIdeaToList() {
     Firebase.database()
       .ref("/ideas").push()
@@ -62,22 +87,52 @@ function App() {
     console.log("DATA SAVED")
   };
 
-  return (
-    <Router>
-    <div className="App">
-    <div style={appHeader}>Idea Tambola</div>
-      <Switch>
-      <Route path="/newidea">
-            <NewIdea/>
-          </Route>
+  //Helper functions  
+  function getLocation()
+  { 
+    let pathLocation = window.location.pathname
+    console.log(pathLocation)
+    return pathLocation
+  }
 
-      <Route path="/">
-       {isBusy ?
-      <div/> : <IdeaDisplay ideaData={masterIdeaList}/>}
-      </Route>
+  function setAppPageState(){
+
+    const activePath = getLocation()
+    console.log(activePath)
+    setActivePage(activePath)
+  }
+
+  //Component stuff 
+  const sampleIdeaData = SampleIdea()
+  const [masterIdeaList, setMasterIdeaList] = useState([]);
+  const [loadingIdeas, setLoadingIdeas] = useState(true);
+  const [activePage, setActivePage] = useState("")
+
+  return (
+  <Router>
+    <div className="App">
+    <div style={appHeader}>
+      <div style={appTitle}>Idea Tambola</div>
+      <div style={navHeader}>
+        <div style={navItemContainer}>
+          <NavLink exact to="/" className="navItemUnselected" activeClassName="navItemSelected">View</NavLink>
+        </div>
+        <div style={navItemContainer}>
+          <NavLink to="/newidea" activeClassName="navItemSelected">Submit</NavLink>
+        </div>
+      </div>
+    </div>
+      <Switch>
+        <Route path="/newidea">
+          <NewIdea/>
+        </Route>
+        <Route path="/">
+        
+          {loadingIdeas ? <div/> : <IdeaDisplay ideaData={masterIdeaList}/>}
+        </Route>
       </Switch>
     </div>
-    </Router>
+  </Router>
   );
 }
 
